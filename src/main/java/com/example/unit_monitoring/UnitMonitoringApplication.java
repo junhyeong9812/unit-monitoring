073e10d07530f232f4d76a4be3e8ebed860a6870
jun <pickjog@gmail.com>
2025-12-04 12:34:32 +0900

    feat: 프로젝트 주석 추가

--- src/main/java/com/example/unit_monitoring/UnitMonitoringApplication.java
+++ src/main/java/com/example/unit_monitoring/UnitMonitoringApplication.java
@@ -3,11 +3,19 @@ package com.example.unit_monitoring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Unit Monitoring Application
+ *
+ * Spring Boot 4.0 기반 모니터링 데모 애플리케이션
+ * - Prometheus 메트릭 수집
+ * - Grafana 시각화
+ * - Loki 로그 집계
+ * - Alertmanager 알림
+ */
 @SpringBootApplication
 public class UnitMonitoringApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UnitMonitoringApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(UnitMonitoringApplication.class, args);
+  }
 }
