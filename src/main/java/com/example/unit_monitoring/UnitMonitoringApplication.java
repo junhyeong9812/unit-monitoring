a3fdc770745c1d4d1b65978eb076ec37b489cef8
jun <pickjog@gmail.com>
2025-12-04 09:50:48 +0900

    자바 25 슛!

---
+++ src/main/java/com/example/unit_monitoring/UnitMonitoringApplication.java
@@ -0,0 +1,13 @@
+package com.example.unit_monitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitMonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnitMonitoringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/unit_monitoring/UnitMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.unit_monitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
